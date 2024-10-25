ffcc4c2c05c62a5df2d2a32b22b5a81143459c57
Styfal <dragonlink999@gmail.com>
2024-10-26 08:28:21 +0900

    Navbar Erased

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import NavBar from "@/components/NavBar";
 import Footer from "@/components/Footer";
 import Provider from "./_trpc/Provider";
 
@@ -20,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}><NavBar/>
+      <body className={inter.className}>
       <Provider>{children}
         </Provider><Footer /></body>
     </html>
