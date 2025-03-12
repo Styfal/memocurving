3f5c35215e7683b00833710fd499d36ad67d6b0f
memocurveito <memocurve.ito@gmail.com>
2025-03-12 22:25:27 +0900

    v3

--- src/app/create/test/page.tsx
+++ src/app/create/test/page.tsx
@@ -1,13 +1,28 @@
+// "use client"
+
+// import TestCreate from "@/components/test-create"
+
+// export default function TestCreatePage() {
+//   return (
+//     <div className="p-4">
+//       <TestCreate />
+//     </div>
+//   )
+// }
+
+
+
 "use client"
 
-import TestCreate from "@/components/test-create"
+import { useState } from 'react'
+import TestCreate, { FreeTestSet, FlashcardTestSet } from "@/components/test-create"
 
 export default function TestCreatePage() {
+  const [testSets, setTestSets] = useState<(FreeTestSet | FlashcardTestSet)[]>([])
+
   return (
     <div className="p-4">
-      <TestCreate />
+      <TestCreate setTestSets={setTestSets} />
     </div>
   )
 }
-
-
