ffacfe91950d9f90e3dcd74b4d0c428767f7b7a7
memocurveito <memocurve.ito@gmail.com>
2025-03-12 21:22:46 +0900

    vercel fix 2

--- src/app/create/test/page.tsx
+++ src/app/create/test/page.tsx
@@ -1,11 +1,28 @@
+// "use client"
+
+// import TestCreate from "@/components/test-create"
+
+// export default function TestCreatePage() {
+//   return (
+//     <div className="p-4">
+//       <TestCreate />
+//     </div>
+//   )
+// }
+
+
+
 "use client"
 
+import { useState } from "react"
 import TestCreate from "@/components/test-create"
 
 export default function TestCreatePage() {
+  const [testSets, setTestSets] = useState<any[]>([])
+
   return (
     <div className="p-4">
-      <TestCreate />
+      <TestCreate setTestSets={setTestSets} />
     </div>
   )
 }
